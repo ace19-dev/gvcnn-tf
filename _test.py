27d4da2fb22dddd0b6ebf47c9163ca5a62722374
ace19-dev <acemc19@gmail.com>
2018-11-27 23:42:54 +0900

    update reference

---
+++ _test.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os,sys,inspect
+import tensorflow as tf
+import time
+from datetime import datetime
+import os
+import os.path as osp
+import sklearn.metrics as metrics
+import math
+
+from _input import Dataset
+import _globals as g_
+
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import model
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('train_dir', osp.dirname(sys.argv[0]) + '/tmp/',
+                           """Directory where to write event logs """
+                           """and checkpoint.""")
+tf.app.flags.DEFINE_boolean('log_device_placement', False,
+                            """Whether to log device placement.""")
+tf.app.flags.DEFINE_string('weights', '',
+                            """finetune with a pretrained model""")
+
+np.set_printoptions(precision=3)
+
+
+def test(dataset, ckptfile):
+    print('test() called')
+    V = g_.NUM_VIEWS
+    batch_size = FLAGS.batch_size
+
+    data_size = dataset.size()
+    print('dataset size:', data_size)
+
+    with tf.Graph().as_default():
+        startstep = 0
+        global_step = tf.Variable(startstep, trainable=False)
+
+        view_ = tf.placeholder('float32', shape=(None, V, 227, 227, 3), name='im0')
+        y_ = tf.placeholder('int64', shape=(None), name='y')
+        keep_prob_ = tf.placeholder('float32')
+
+        fc8 = model.inference_multiview(view_, g_.NUM_CLASSES, keep_prob_)
+        loss = model.loss(fc8, y_)
+        train_op = model.train(loss, global_step, data_size)
+        prediction = model.classify(fc8)
+
+        saver = tf.train.Saver(tf.all_variables(), max_to_keep=1000)
+
+        init_op = tf.global_variables_initializer()
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=FLAGS.log_device_placement))
+
+        saver.restore(sess, ckptfile)
+        print('restore variables done')
+
+        step = startstep
+
+        predictions = []
+        labels = []
+
+        print("Start testing")
+        print("Size:", data_size)
+        print("It'll take", int(math.ceil(data_size/batch_size)), "iterations.")
+
+        for batch_x, batch_y in dataset.batches(batch_size):
+            step += 1
+
+            start_time = time.time()
+            feed_dict = {view_: batch_x,
+                         y_ : batch_y,
+                         keep_prob_: 1.0}
+
+            pred, loss_value = sess.run(
+                    [prediction,  loss,],
+                    feed_dict=feed_dict)
+
+
+            duration = time.time() - start_time
+
+            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            if step % 10 == 0:
+                sec_per_batch = float(duration)
+                print('%s: step %d, loss=%.2f (%.1f examples/sec; %.3f sec/batch)' \
+                     % (datetime.now(), step, loss_value,
+                                FLAGS.batch_size/duration, sec_per_batch))
+
+            predictions.extend(pred.tolist())
+            labels.extend(batch_y.tolist())
+
+        # print(labels)
+        # print(predictions)
+        acc = metrics.accuracy_score(labels, predictions)
+        print('acc:', acc*100)
+
+
+def main(argv):
+    st = time.time()
+    print('start loading data')
+
+    listfiles, labels = read_lists(g_.TEST_LOL)
+    dataset = Dataset(listfiles, labels, subtract_mean=False, V=g_.NUM_VIEWS)
+
+    print('done loading data, time=', time.time() - st)
+
+    test(dataset, FLAGS.weights)
+
+
+def read_lists(list_of_lists_file):
+    listfile_labels = np.loadtxt(list_of_lists_file, dtype=str).tolist()
+    listfiles, labels  = zip(*[(l[0], int(l[1])) for l in listfile_labels])
+    return listfiles, labels
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
--- train.py
+++ train.py
@@ -1,14 +1,27 @@
-import tensorflow as tf
+import time
+import numpy as np
 
+import tensorflow as tf
 import model
+from utils import train_utils
 
 from slim.deployment import model_deploy
 
 slim = tf.contrib.slim
 
+prefetch_queue = slim.prefetch_queue
+
 flags = tf.app.flags
 
 FLAGS = flags.FLAGS
+
+
+LABELS_CLASS = 'labels_class'
+IMAGE = 'image'
+HEIGHT = 'height'
+WIDTH = 'width'
+IMAGE_NAME = 'image_name'
+LABEL = 'label'
 
 
 flags.DEFINE_integer('num_clones', 1, 'Number of clones to deploy.')
@@ -66,16 +79,15 @@ flags.DEFINE_string('dataset_dir', './datasets/pascal_voc_seg/tfrecord',
                     'Where the dataset reside.')
 
 
-def main(unused_argv):
-    tf.logging.set_verbosity(tf.logging.INFO)
 
+def test():
     train_batch_size = 1
     eval_batch_size = 2
     height, width = 224, 224
     num_classes = 1000
 
     inputs = []
-    for i in range(1,5):
+    for i in range(1, 5):
         train_inputs = tf.random_uniform((train_batch_size, height, width, 3))
         inputs.append(train_inputs)
 
@@ -83,169 +95,195 @@ def main(unused_argv):
     discrimination_logits = model.grouping_module(fcn_nets)
 
     with tf.Session() as sess:
-      sess.run(tf.global_variables_initializer())
-      output = sess.run(discrimination_logits[0])
-      tf.logging.info("logging -> ", output)
-      # tf.test.TestCase.assertEquals(output.shape, (batch_size,))
+        sess.run(tf.global_variables_initializer())
+        output = sess.run(discrimination_logits[0])
+        tf.logging.info("logging -> ", output)
+        # tf.test.TestCase.assertEquals(output.shape, (batch_size,))
 
 
-    # # Set up deployment (i.e. multi-GPUs and/or multi-replicas).
-    # config = model_deploy.DeploymentConfig(
-    #     num_clones=FLAGS.num_clones,
-    #     clone_on_cpu=FLAGS.clones_on_cpu,
-    #     replica_id=FLAGS.task,
-    #     num_replicas=FLAGS.num_replicas,
-    #     num_ps_tasks=FLAGS.num_ps_tasks)
-    #
-    # # Split the batch across GPUs.
-    # assert FLAGS.train_batch_size % config.num_clones == 0, (
-    #     'Training batch size not divisible by number of clones (GPUs).')
-    #
-    # clone_batch_size = FLAGS.train_batch_size // config.num_clones
-    #
-    # # Get dataset infomation
-    #
-    #
-    # tf.gfile.MakeDirs(FLAGS.train_logdir)
-    # tf.logging.info('Training on %s set', FLAGS.train_split)
-    #
-    # with tf.Graph().as_default() as graph:
-    #     with tf.device(config.inputs_device()):
-    #         samples = input_generator.get(
-    #             dataset,
-    #             FLAGS.train_crop_size,
-    #             clone_batch_size,
-    #             min_resize_value=FLAGS.min_resize_value,
-    #             max_resize_value=FLAGS.max_resize_value,
-    #             resize_factor=FLAGS.resize_factor,
-    #             min_scale_factor=FLAGS.min_scale_factor,
-    #             max_scale_factor=FLAGS.max_scale_factor,
-    #             scale_factor_step_size=FLAGS.scale_factor_step_size,
-    #             dataset_split=FLAGS.train_split,
-    #             is_training=True,
-    #             model_variant=FLAGS.model_variant)
-    #         inputs_queue = prefetch_queue.prefetch_queue(
-    #             samples, capacity=128 * config.num_clones)
-    #
-    #     # Create the global step on the device storing the variables.
-    #     with tf.device(config.variables_device()):
-    #         global_step = tf.train.get_or_create_global_step()
-    #
-    #         # Define the model and create clones.
-    #         model_fn = _build_deeplab
-    #         model_args = (inputs_queue, {
-    #             common.OUTPUT_TYPE: dataset.num_classes
-    #         }, dataset.ignore_label)
-    #         clones = model_deploy.create_clones(config, model_fn, args=model_args)
-    #
-    #         # Gather update_ops from the first clone. These contain, for example,
-    #         # the updates for the batch_norm variables created by model_fn.
-    #         first_clone_scope = config.clone_scope(0)
-    #         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, first_clone_scope)
-    #
-    #     # Gather initial summaries.
-    #     summaries = set(tf.get_collection(tf.GraphKeys.SUMMARIES))
-    #
-    #     # Add summaries for model variables.
-    #     for model_var in slim.get_model_variables():
-    #         summaries.add(tf.summary.histogram(model_var.op.name, model_var))
-    #
-    #     # Add summaries for images, labels, semantic predictions
-    #     if FLAGS.save_summaries_images:
-    #         summary_image = graph.get_tensor_by_name(
-    #             ('%s/%s:0' % (first_clone_scope, common.IMAGE)).strip('/'))
-    #         summaries.add(
-    #             tf.summary.image('samples/%s' % common.IMAGE, summary_image))
-    #
-    #         first_clone_label = graph.get_tensor_by_name(
-    #             ('%s/%s:0' % (first_clone_scope, common.LABEL)).strip('/'))
-    #         # Scale up summary image pixel values for better visualization.
-    #         pixel_scaling = max(1, 255 // dataset.num_classes)
-    #         summary_label = tf.cast(first_clone_label * pixel_scaling, tf.uint8)
-    #         summaries.add(
-    #             tf.summary.image('samples/%s' % common.LABEL, summary_label))
-    #
-    #         first_clone_output = graph.get_tensor_by_name(
-    #             ('%s/%s:0' % (first_clone_scope, common.OUTPUT_TYPE)).strip('/'))
-    #         predictions = tf.expand_dims(tf.argmax(first_clone_output, 3), -1)
-    #
-    #         summary_predictions = tf.cast(predictions * pixel_scaling, tf.uint8)
-    #         summaries.add(
-    #             tf.summary.image(
-    #                 'samples/%s' % common.OUTPUT_TYPE, summary_predictions))
-    #
-    #     # Add summaries for losses.
-    #     for loss in tf.get_collection(tf.GraphKeys.LOSSES, first_clone_scope):
-    #         summaries.add(tf.summary.scalar('losses/%s' % loss.op.name, loss))
-    #
-    #     # Build the optimizer based on the device specification.
-    #     with tf.device(config.optimizer_device()):
-    #         learning_rate = train_utils.get_model_learning_rate(
-    #             FLAGS.learning_policy, FLAGS.base_learning_rate,
-    #             FLAGS.learning_rate_decay_step, FLAGS.learning_rate_decay_factor,
-    #             FLAGS.training_number_of_steps, FLAGS.learning_power,
-    #             FLAGS.slow_start_step, FLAGS.slow_start_learning_rate)
-    #         optimizer = tf.train.MomentumOptimizer(learning_rate, FLAGS.momentum)
-    #         summaries.add(tf.summary.scalar('learning_rate', learning_rate))
-    #
-    #     startup_delay_steps = FLAGS.task * FLAGS.startup_delay_steps
-    #     for variable in slim.get_model_variables():
-    #         summaries.add(tf.summary.histogram(variable.op.name, variable))
-    #
-    #     with tf.device(config.variables_device()):
-    #         total_loss, grads_and_vars = model_deploy.optimize_clones(
-    #             clones, optimizer)
-    #         total_loss = tf.check_numerics(total_loss, 'Loss is inf or nan.')
-    #         summaries.add(tf.summary.scalar('total_loss', total_loss))
-    #
-    #         # Modify the gradients for biases and last layer variables.
-    #         last_layers = model.get_extra_layer_scopes(
-    #             FLAGS.last_layers_contain_logits_only)
-    #         grad_mult = train_utils.get_model_gradient_multipliers(
-    #             last_layers, FLAGS.last_layer_gradient_multiplier)
-    #         if grad_mult:
-    #             grads_and_vars = slim.learning.multiply_gradients(
-    #                 grads_and_vars, grad_mult)
-    #
-    #         # Create gradient update op.
-    #         grad_updates = optimizer.apply_gradients(
-    #             grads_and_vars, global_step=global_step)
-    #         update_ops.append(grad_updates)
-    #         update_op = tf.group(*update_ops)
-    #         with tf.control_dependencies([update_op]):
-    #             train_tensor = tf.identity(total_loss, name='train_op')
-    #
-    #     # Add the summaries from the first clone. These contain the summaries
-    #     # created by model_fn and either optimize_clones() or _gather_clone_loss().
-    #     summaries |= set(
-    #         tf.get_collection(tf.GraphKeys.SUMMARIES, first_clone_scope))
-    #
-    #     # Merge all summaries together.
-    #     summary_op = tf.summary.merge(list(summaries))
-    #
-    #     # Soft placement allows placing on CPU ops without GPU implementation.
-    #     session_config = tf.ConfigProto(
-    #         allow_soft_placement=True, log_device_placement=False)
-    #
-    #     # Start the training.
-    #     slim.learning.train(
-    #         train_tensor,
-    #         logdir=FLAGS.train_logdir,
-    #         log_every_n_steps=FLAGS.log_steps,
-    #         master=FLAGS.master,
-    #         number_of_steps=FLAGS.training_number_of_steps,
-    #         is_chief=(FLAGS.task == 0),
-    #         session_config=session_config,
-    #         startup_delay_steps=startup_delay_steps,
-    #         init_fn=train_utils.get_model_init_fn(
-    #             FLAGS.train_logdir,
-    #             FLAGS.tf_initial_checkpoint,
-    #             FLAGS.initialize_last_layer,
-    #             last_layers,
-    #             ignore_missing_vars=True),
-    #         summary_op=summary_op,
-    #         save_summaries_secs=FLAGS.save_summaries_secs,
-    #         save_interval_secs=FLAGS.save_interval_secs)
+def read_lists(list_of_lists_file):
+    listfile_labels = np.loadtxt(list_of_lists_file, dtype=str).tolist()
+    listfiles, labels  = zip(*[(l[0], int(l[1])) for l in listfile_labels])
+    return listfiles, labels
+
+
+def main(unused_argv):
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    # test()
+
+    # Set up deployment (i.e. multi-GPUs and/or multi-replicas).
+    config = model_deploy.DeploymentConfig(
+        num_clones=FLAGS.num_clones,
+        clone_on_cpu=FLAGS.clones_on_cpu,
+        replica_id=FLAGS.task,
+        num_replicas=FLAGS.num_replicas,
+        num_ps_tasks=FLAGS.num_ps_tasks)
+
+    # Split the batch across GPUs.
+    assert FLAGS.train_batch_size % config.num_clones == 0, (
+        'Training batch size not divisible by number of clones (GPUs).')
+
+    clone_batch_size = FLAGS.train_batch_size // config.num_clones
+
+    # Get dataset infomation
+    st = time.time()
+    print('start loading data')
+
+    listfiles_train, labels_train = read_lists(g_.TRAIN_LOL)
+    listfiles_val, labels_val = read_lists(g_.VAL_LOL)
+    dataset_train = Dataset(listfiles_train, labels_train, subtract_mean=False, V=g_.NUM_VIEWS)
+    dataset_val = Dataset(listfiles_val, labels_val, subtract_mean=False, V=g_.NUM_VIEWS)
+
+    print('done loading data, time=', time.time() - st)
+
+
+    tf.gfile.MakeDirs(FLAGS.train_logdir)
+    tf.logging.info('Training on %s set', FLAGS.train_split)
+
+    with tf.Graph().as_default() as graph:
+        with tf.device(config.inputs_device()):
+            samples = input_generator.get(
+                dataset,
+                FLAGS.train_crop_size,
+                clone_batch_size,
+                min_resize_value=FLAGS.min_resize_value,
+                max_resize_value=FLAGS.max_resize_value,
+                resize_factor=FLAGS.resize_factor,
+                min_scale_factor=FLAGS.min_scale_factor,
+                max_scale_factor=FLAGS.max_scale_factor,
+                scale_factor_step_size=FLAGS.scale_factor_step_size,
+                dataset_split=FLAGS.train_split,
+                is_training=True,
+                model_variant=FLAGS.model_variant)
+            inputs_queue = prefetch_queue.prefetch_queue(
+                samples, capacity=128 * config.num_clones)
+
+        # Create the global step on the device storing the variables.
+        with tf.device(config.variables_device()):
+            global_step = tf.train.get_or_create_global_step()
+
+            # graph outputs
+            # fc8 = model.inference_multiview(view_, g_.NUM_CLASSES, keep_prob_)
+            # loss = model.loss(fc8, y_)
+            # train_op = model.train(loss, global_step, data_size)
+            # prediction = model.classify(fc8)
+
+            # Define the model and create clones.
+            model_fn = model.FCN
+            model_args = (inputs_queue, {
+                common.OUTPUT_TYPE: dataset.num_classes
+            }, dataset.ignore_label)
+            clones = model_deploy.create_clones(config, model_fn, args=model_args)
+
+            # Gather update_ops from the first clone. These contain, for example,
+            # the updates for the batch_norm variables created by model_fn.
+            first_clone_scope = config.clone_scope(0)
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, first_clone_scope)
+
+        # Gather initial summaries.
+        summaries = set(tf.get_collection(tf.GraphKeys.SUMMARIES))
+
+        # Add summaries for model variables.
+        for model_var in slim.get_model_variables():
+            summaries.add(tf.summary.histogram(model_var.op.name, model_var))
+
+        # Add summaries for images, labels, semantic predictions
+        if FLAGS.save_summaries_images:
+            summary_image = graph.get_tensor_by_name(
+                ('%s/%s:0' % (first_clone_scope, IMAGE)).strip('/'))
+            summaries.add(
+                tf.summary.image('samples/%s' % IMAGE, summary_image))
+
+            first_clone_label = graph.get_tensor_by_name(
+                ('%s/%s:0' % (first_clone_scope, LABEL)).strip('/'))
+            # Scale up summary image pixel values for better visualization.
+            pixel_scaling = max(1, 255 // dataset.num_classes)
+            summary_label = tf.cast(first_clone_label * pixel_scaling, tf.uint8)
+            summaries.add(
+                tf.summary.image('samples/%s' % LABEL, summary_label))
+
+            first_clone_output = graph.get_tensor_by_name(
+                ('%s/%s:0' % (first_clone_scope, common.OUTPUT_TYPE)).strip('/'))
+            predictions = tf.expand_dims(tf.argmax(first_clone_output, 3), -1)
+
+            summary_predictions = tf.cast(predictions * pixel_scaling, tf.uint8)
+            summaries.add(
+                tf.summary.image(
+                    'samples/%s' % common.OUTPUT_TYPE, summary_predictions))
+
+        # Add summaries for losses.
+        for loss in tf.get_collection(tf.GraphKeys.LOSSES, first_clone_scope):
+            summaries.add(tf.summary.scalar('losses/%s' % loss.op.name, loss))
+
+        # Build the optimizer based on the device specification.
+        with tf.device(config.optimizer_device()):
+            learning_rate = train_utils.get_model_learning_rate(
+                FLAGS.learning_policy, FLAGS.base_learning_rate,
+                FLAGS.learning_rate_decay_step, FLAGS.learning_rate_decay_factor,
+                FLAGS.training_number_of_steps, FLAGS.learning_power,
+                FLAGS.slow_start_step, FLAGS.slow_start_learning_rate)
+            optimizer = tf.train.MomentumOptimizer(learning_rate, FLAGS.momentum)
+            summaries.add(tf.summary.scalar('learning_rate', learning_rate))
+
+        startup_delay_steps = FLAGS.task * FLAGS.startup_delay_steps
+        for variable in slim.get_model_variables():
+            summaries.add(tf.summary.histogram(variable.op.name, variable))
+
+        with tf.device(config.variables_device()):
+            total_loss, grads_and_vars = model_deploy.optimize_clones(
+                clones, optimizer)
+            total_loss = tf.check_numerics(total_loss, 'Loss is inf or nan.')
+            summaries.add(tf.summary.scalar('total_loss', total_loss))
+
+            # Modify the gradients for biases and last layer variables.
+            last_layers = model.get_extra_layer_scopes(
+                FLAGS.last_layers_contain_logits_only)
+            grad_mult = train_utils.get_model_gradient_multipliers(
+                last_layers, FLAGS.last_layer_gradient_multiplier)
+            if grad_mult:
+                grads_and_vars = slim.learning.multiply_gradients(
+                    grads_and_vars, grad_mult)
+
+            # Create gradient update op.
+            grad_updates = optimizer.apply_gradients(
+                grads_and_vars, global_step=global_step)
+            update_ops.append(grad_updates)
+            update_op = tf.group(*update_ops)
+            with tf.control_dependencies([update_op]):
+                train_tensor = tf.identity(total_loss, name='train_op')
+
+        # Add the summaries from the first clone. These contain the summaries
+        # created by model_fn and either optimize_clones() or _gather_clone_loss().
+        summaries |= set(
+            tf.get_collection(tf.GraphKeys.SUMMARIES, first_clone_scope))
+
+        # Merge all summaries together.
+        summary_op = tf.summary.merge(list(summaries))
+
+        # Soft placement allows placing on CPU ops without GPU implementation.
+        session_config = tf.ConfigProto(
+            allow_soft_placement=True, log_device_placement=False)
+
+        # Start the training.
+        slim.learning.train(
+            train_tensor,
+            logdir=FLAGS.train_logdir,
+            log_every_n_steps=FLAGS.log_steps,
+            master=FLAGS.master,
+            number_of_steps=FLAGS.training_number_of_steps,
+            is_chief=(FLAGS.task == 0),
+            session_config=session_config,
+            startup_delay_steps=startup_delay_steps,
+            init_fn=train_utils.get_model_init_fn(
+                FLAGS.train_logdir,
+                FLAGS.tf_initial_checkpoint,
+                FLAGS.initialize_last_layer,
+                last_layers,
+                ignore_missing_vars=True),
+            summary_op=summary_op,
+            save_summaries_secs=FLAGS.save_summaries_secs,
+            save_interval_secs=FLAGS.save_interval_secs)
 
 
 
