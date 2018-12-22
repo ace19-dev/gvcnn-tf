b538a753b0394ec1888064917cb6c44fa553d8aa
Sean <acemc19@gmail.com>
2018-12-22 23:32:23 +0900

    update

---
+++ utils/make_view_dir.py
@@ -0,0 +1,30 @@
+import os
+
+import tensorflow as tf
+
+flags = tf.app.flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('target_dir', '/home/ace19/dl_data/modelnet5_low/vase/test',
+                    'target_dir')
+
+flags.DEFINE_string('source', '/home/ace19/dl_data/ModelNet40/vase/test',
+                    'source')
+
+
+def main(unused_argv):
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    off_files = os.listdir(FLAGS.source)
+    off_files.sort()
+
+    for f in off_files:
+        if '.off' in f:
+            path = os.path.join(FLAGS.target_dir, f)
+            os.makedirs(path)
+
+
+
+if __name__ == '__main__':
+    tf.app.run()
