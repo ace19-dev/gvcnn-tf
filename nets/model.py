f94d5551e8a69291ce0bd58f96913a6ac7763bc6
ace19-dev <acemc19@gmail.com>
2019-11-21 14:49:17 +0900

    on debugging

--- nets/model.py
+++ nets/model.py
@@ -151,6 +151,7 @@ def gvcnn(inputs, num_classes, group_scheme, group_weight,
         final_view_descriptors.append(end_points['resnet_v2_50/block4'])
 
     # TODO: checkpoint - problem happens on block below.
+    #       why is validation accuracy not working?
     # -----------------------------
     # Intra-Group View Pooling
     group_descriptors = view_pooling(final_view_descriptors, group_scheme)
